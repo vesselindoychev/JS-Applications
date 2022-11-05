8600b08dd19980cf15459e0d253c22b765860877
Veselin Doychev <veselindoi123@gmail.com>
2022-11-05 18:36:16 +0200

    Created Remote-Data-and-Authentication-Exercise and added some tasks and solved it

--- 04-Remote-Data-and-Authentication-Exercise/01.Messenger/app.js
+++ 04-Remote-Data-and-Authentication-Exercise/01.Messenger/app.js
@@ -1,5 +1,53 @@
 function attachEvents() {
-    console.log('TODO...');
+    let refreshBtn = document.getElementById('refresh');
+    let sendBtn = document.getElementById('submit');
+
+    refreshBtn.addEventListener('click', loadMessages);
+    sendBtn.addEventListener('click', postMessage);
+}
+
+function renderMessage(data) {
+    let messageChat = document.getElementById('messages');
+    let messagesArray = [];
+    if (messageChat.textContent.length > 0) {
+        messageChat.textContent = '';
+    }
+    for (let obj of Object.values(data)) {
+        messagesArray.push(`${obj.author}: ${obj.content}`)
+    }
+    messageChat.textContent += messagesArray.join('\n');
+}
+
+async function loadMessages() {
+    const response = await fetch(`http://localhost:3030/jsonstore/messenger`);
+    const data = await response.json();
+    renderMessage(data);    
+}
+
+async function onPost(author, content) {
+        
+    const response = await fetch(`http://localhost:3030/jsonstore/messenger`, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ author, content}) 
+    }) 
+    // either loadMessages() or this syntax under the function
+    loadMessages()
+    // const data = await response.json();
+    // return data;    
+}
+
+function postMessage() {
+    let nameInput = document.getElementsByName('author')[0];
+    let messageInput = document.getElementsByName('content')[0];
+    author = nameInput.value;
+    content = messageInput.value;
+
+    onPost(author, content);
+    nameInput.value = '';
+    messageInput.value = '';
 }
 
 attachEvents();
--- 04-Remote-Data-and-Authentication-Exercise/02.Phonebook/app.js
+++ 04-Remote-Data-and-Authentication-Exercise/02.Phonebook/app.js
@@ -1,5 +1,71 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('btnLoad').addEventListener('click', getAllContacts);
+    document.getElementById('btnCreate').addEventListener('click', createEntry);
+
+}
+
+async function createEntry() {
+    let personNameInput = document.getElementById('person');
+    let phoneNumberInput = document.getElementById('phone');
+
+    let person = personNameInput.value;
+    let phone = phoneNumberInput.value;
+    const response = await fetch(`http://localhost:3030/jsonstore/phonebook`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ person, phone })
+    });
+    
+    getAllContacts();
+    personNameInput.value = '';
+    phoneNumberInput.value = '';
+}
+
+function renderContacts(data) {
+    let phoneBookUl = document.getElementById('phonebook');
+
+    if (phoneBookUl.children) {
+        phoneBookUl.innerHTML = '';
+    } 
+    let phoneBookEntries = Object.values(data);
+    for (let contact of phoneBookEntries) {
+        let li = document.createElement('li');
+        li.textContent = `${contact.person}: ${contact.phone}`
+        li.setAttribute('id', contact._id);
+        let delBtn = document.createElement('button');
+        delBtn.textContent = 'Delete';
+        delBtn.addEventListener('click', deleteEntry);
+        li.appendChild(delBtn)
+        phoneBookUl.appendChild(li);
+    }
+    
+}
+
+async function getAllContacts() {
+    const response = await fetch(`http://localhost:3030/jsonstore/phonebook`);
+    const data = await response.json();
+    renderContacts(data);
+    
+}
+
+function onDelete(data) {
+    let phoneBookUl = document.getElementById('phonebook');
+    let currentLi = Array.from(phoneBookUl.children).find(li => li.getAttribute('id') == data._id);
+    currentLi.remove();
+}
+
+async function deleteEntry(event) {
+    let key = event.target.parentElement.getAttribute('id');
+    const response = await fetch(`http://localhost:3030/jsonstore/phonebook/${key}`, {
+        method: 'delete',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const data = await response.json();
+    onDelete(data);
 }
 
 attachEvents();
